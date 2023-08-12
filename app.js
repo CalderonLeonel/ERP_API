6446095bffcfba0b3e7ba2ebc49d154948c96039
CalderonLeonel <crl1009575@est.univalle.edu>
2023-08-12 14:05:23 -0400

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import history from 'connect-history-api-fallback';
+import path from 'path';
+import multer from 'multer';
+import sharp from 'sharp';
+
+
+
+//const express require('express');
+//const morgan require('morgan');
+//const cors require('cors');
+//const fileUpload require('express-fileupload');
+//const history require('connect-history-api-fallback');
+//const path require('path');
+
+const app = express();
+
+//MIDELWARES 
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(fileUpload({useTempFiles:true}));
+
+//ROUTES
+//app.use('/', require('./routes/auth.routes'));
+
+//MIDELWARES FOR VUE
+app.use(history());
+app.use('/uploads', express.static('uploads'))
+
+// SETTINGS
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log('EL PUERTO DEL SERVIDOR ES ' + app.get('port'));
+})
+
---
+++ routes/linea.routes.js
@@ -0,0 +1,4 @@
+import { express } from "express";
+import lineas from '../controllers/lin';
+
+const router = express.Router();
