5ba750ef4252fa52499719c5058d93c1e484daf8
Brandon Montoya <MGJ2019761@est.univalle.edu>
2023-08-15 07:49:45 -0400

    Carpeta Controladores

---
+++ controllers/prod.js
@@ -0,0 +1,20 @@
+import pool from "../database/Keys";
+const productos = {};
+
+productos.listarProductos = async (req, res) => {
+  try {
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
