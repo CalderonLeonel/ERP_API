32aa6f9b703b379563dab275a144b02ea71f0660
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-20 12:58:09 -0400

    Agregando controlador de almacenes

---
+++ controllers/alm.js
@@ -0,0 +1,23 @@
+import pool from "../database/Keys";
+const almacen = {};
+
+almacen.listarAlmacenes = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_almacen()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
+module.exports = almacen;
