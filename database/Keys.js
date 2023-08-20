7bd18fe6e1f6fba95232b14c6aaf97b57178686c
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-20 13:01:54 -0400

    Actualizando credenciales de la base de datos

--- database/Keys.js
+++ database/Keys.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     host : '127.0.0.1',
     port : '5432',
     user : 'postgres',
-    password : 'developerlover69',
+    password : 'root',
     database :'erp_produccion'
 });
 
