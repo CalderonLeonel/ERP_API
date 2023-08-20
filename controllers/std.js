e5f6f53daa16879f85c2d096f40b4bcecf80c617
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-20 12:58:53 -0400

    Agregando controlador de stands

---
+++ controllers/std.js
@@ -0,0 +1,23 @@
+import pool from "../database/Keys";
+const stand = {};
+
+stand.listarStands = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_stand()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
+module.exports = stand;
