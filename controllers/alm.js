9169f72c7d7d11f6d45a7c31eab92acc916c63f1
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-21 01:26:09 -0400

    Creando controlador de almacenes

--- controllers/alm.js
+++ controllers/alm.js
@@ -20,4 +20,26 @@ almacen.listarAlmacenes = async (req, res) => {
   }
 };
 
+
+//Almacenamiento
+
+almacen.listarAlmacenamiento = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_almacenamiento()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
 module.exports = almacen;
