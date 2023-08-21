0c0760bcfc23c8a9e8bb4784accfd65098b13100
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-21 18:12:46 -0400

    Agregando la funcion agregarProveedor

--- controllers/prov.js
+++ controllers/prov.js
@@ -20,4 +20,31 @@ proveedor.listarproveedores = async (req, res) => {
   }
 };
 
+
+proveedor.agregarProveedor = async(req,res) =>{
+  const nombre = req.params.p1;
+  const contacto1 = req.params.p2;
+  const contacto2= req.params.p3;
+  const correo = req.params.p4;
+  const est  = req.params.p5;
+  
+  
+   try {
+         await pool.query("select erp_produccion.erp_insertar_proveedor($1,$2,$3,$4,$5)",[nombre,contacto1,contacto2,correo,est]);
+                           
+             res.status(200).json({
+                 message:'SE GUARDARON LOS CAMBIOS :)'
+           
+             })
+         
+                    
+     } catch (error) {
+         res.status(500).json({
+             message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+             error
+         })
+     }
+ 
+ };
+
 module.exports = proveedor;
