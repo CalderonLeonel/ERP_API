bfb04e4ffa31ef802c17ff88f039b3a99cf215f1
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-23 11:54:57 -0400

    Agregando funcion para actualizar cotizacion_item

--- controllers/adq.js
+++ controllers/adq.js
@@ -3,7 +3,7 @@ const adquisicion = {};
 
 //cotizacionAdquisicion
 
-adquisicion.listarcotizaciondeadquisicion = async (req, res) => {
+adquisicion.listarcotizacionadquisicion = async (req, res) => {
   try {
     const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_cotizaciondeadquisicion()")).rows;
     if (resultado.length > 0) {
@@ -23,7 +23,7 @@ adquisicion.listarcotizaciondeadquisicion = async (req, res) => {
 };
 
 
-adquisicion.agregarcotizaciondeadquisicion  = async(req,res) =>{
+adquisicion.agregarcotizacionadquisicion  = async(req,res) =>{
   const id_usuario = req.params.p1;
   const id_proveedor = req.params.p2;
   const nombreCotizacion= req.params.p3;
@@ -48,7 +48,7 @@ adquisicion.agregarcotizaciondeadquisicion  = async(req,res) =>{
  
  };
 
- adquisicion.actualizarcotizaciondeadquisicion  = async(req,res) =>{
+ adquisicion.actualizarcotizacionadquisicion  = async(req,res) =>{
   const id = req.params.p1;
   const id_usuario = req.params.p1;
   const id_proveedor = req.params.p2;
@@ -121,15 +121,15 @@ adquisicion.listarcotizacionitem = async (req, res) => {
 };
 
 
-adquisicion.agregarcotizaciondeitem  = async(req,res) =>{
+adquisicion.agregarcotizacionitem  = async(req,res) =>{
   const id_cotizacion = req.params.p1;
   const id_item = req.params.p2;
-  const precioYnitario= req.params.p3;
+  const precioUnitario= req.params.p3;
   const est  = req.params.p5;
   
   
    try {
-         await pool.query("select erp_produccion.erp_insertar_cotizacion_item($1,$2,$3,$4)",[id_cotizacion,id_item,precioYnitario,est]);
+         await pool.query("select erp_produccion.erp_insertar_cotizacion_item($1,$2,$3,$4)",[id_cotizacion,id_item,precioUnitario,est]);
                            
              res.status(200).json({
                  message:'CAMPO GUARDADO CORRECTAMENTE :)'
@@ -146,6 +146,33 @@ adquisicion.agregarcotizaciondeitem  = async(req,res) =>{
  
  };
 
+ adquisicion.actualizarcotizacionitem  = async(req,res) =>{
+  const id = req.params.p1;
+  const id_cotizacion = req.params.p1;
+  const id_item = req.params.p2;
+  const precioUnitario= req.params.p3;
+  const est  = req.params.p5;
+  
+  
+  
+  
+   try {
+         await pool.query("select erp_produccion.erp_actualizar_cotizacion_item(($1,$2,$3,$4,$5)",[id,id_cotizacion,id_item,precioUnitario,est]);
+                           
+             res.status(200).json({
+                 message:'SE GUARDARON LOS CAMBIOS :)'
+           
+             })
+         
+                    
+     } catch (error) {
+         res.status(500).json({
+             message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+             error
+         })
+     }
+ 
+ };
 
 
 module.exports = adquisicion;
