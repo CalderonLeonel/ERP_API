2448257822f61edf9c9d176ff5638fdeae6d7b7e
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-23 10:52:08 -0400

    Agregando la funcion para actualizar cotizacion

--- controllers/adq.js
+++ controllers/adq.js
@@ -31,10 +31,37 @@ adquisicion.agregarcotizaciondeadquisicion  = async(req,res) =>{
   
   
    try {
-         await pool.query("select erp_produccion.erp_insertar_proveedor($1,$2,$3,$4)",[id_usuario,id_proveedor,nombreCotizacion,est]);
+         await pool.query("select erp_produccion.erp_insertar_cotizacion_adquisicion($1,$2,$3,$4)",[id_usuario,id_proveedor,nombreCotizacion,est]);
                            
              res.status(200).json({
                  message:'CAMPO GUARDADO CORRECTAMENTE :)'
+           
+             })
+         
+                    
+     } catch (error) {
+         res.status(500).json({
+             message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+             error
+         })
+     }
+ 
+ };
+
+ adquisicion.actualizarcotizaciondeadquisicion  = async(req,res) =>{
+  const id = req.params.p1;
+  const id_usuario = req.params.p1;
+  const id_proveedor = req.params.p2;
+  const nombreCotizacion= req.params.p3;
+  const est  = req.params.p5;
+  
+  
+  
+   try {
+         await pool.query("select erp_produccion.erp_actualizar_cotizacion_adquisicion($1,$2,$3,$4,$5)",[id,id_usuario,id_proveedor,nombreCotizacion,est]);
+                           
+             res.status(200).json({
+                 message:'SE GUARDARON LOS CAMBIOS :)'
            
              })
          
