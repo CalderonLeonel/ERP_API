931a3cc8ad029a65a554e839ae9fa42af53a4e87
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-23 19:17:12 -0400

    Agregando la funcion actualizar seccion

--- controllers/sec.js
+++ controllers/sec.js
@@ -25,11 +25,38 @@ seccion.listarSecciones = async (req, res) => {
 seccion.agregarSeccion = async(req,res) =>{
   const nombre = req.params.p1;
   const idAlmacen = req.params.p2;
-  const est  = req.params.p5;
+  const est  = req.params.p3;
   
   
    try {
          await pool.query("select erp_produccion.erp_insertar_seccion($1,$2,$3,$4,$5)",[nombre,idAlmacen,est]);
+                           
+             res.status(200).json({
+                 message:'CAMPO GUARDADO CORRECTAMENTE :)'
+           
+             })
+         
+                    
+     } catch (error) {
+         res.status(500).json({
+             message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+             error
+         })
+     }
+ 
+ };
+
+
+
+ seccion.actualizarSeccion = async(req,res) =>{
+  const id = req.params.p1;
+  const nombre = req.params.p2;
+  const idAlmacen  = req.params.p3;
+  const est  = req.params.p3;
+  
+  
+   try {
+         await pool.query("select erp_produccion.erp_actualizar_seccion($1,$2,$3,$4)",[id,nombre,idAlmacen,est]);
                            
              res.status(200).json({
                  message:'CAMPO GUARDADO CORRECTAMENTE :)'
