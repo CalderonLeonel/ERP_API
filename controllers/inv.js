fc23a99e8d75047d730fe54bd3e585e35d24e090
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-23 20:52:10 -0400

    Agregando la funcion actualizar TipoItem

--- controllers/inv.js
+++ controllers/inv.js
@@ -235,6 +235,28 @@ inventario.listarTipoItem = async (req, res) => {
   };
 
 
+  
+  inventario.actualizarTipoItem = async (req, res) => {
+    const id = req.params.p1;
+    const nombre = req.params.p2;
+    const est  = req.params.p3;
+    try {
+      await pool.query("select erp_produccion.erp_actualizar_tipodeitem($1,$2,$3)",[id,nombre,est]);
+                        
+          res.status(200).json({
+              message:'CAMPO GUARDADO CORRECTAMENTE :)'
+        
+          })           
+    } catch (error) {
+        res.status(500).json({
+            message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+            error
+        })
+    }
+  
+  
+  };
+
   inventario.eliminarTipoItem = async(req,res) =>{
     const id = req.params.p1;
   
