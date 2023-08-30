18bcfbff2c6419930cac24966bbe225baed9f1ea
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 10:59:48 -0400

    Agregando Listado de Activos/Inactivos

--- controllers/alm.js
+++ controllers/alm.js
@@ -20,6 +20,44 @@ almacen.listarAlmacenes = async (req, res) => {
   }
 };
 
+almacen.listarAlmacenesActivos = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_almacen_activo()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
+almacen.listarAlmacenesInactivos = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_almacen_inactivo()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
 
 almacen.agregarAlmacen = async(req,res) =>{
   const nombreAlmacen = req.params.p1;
