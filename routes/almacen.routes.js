b47635217a9f768d29d1191f226fbd77d08c616b
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 11:07:17 -0400

    Agregando rutas para la gestion de almacenes

--- routes/almacen.routes.js
+++ routes/almacen.routes.js
@@ -4,6 +4,18 @@ import almacen from '../controllers/alm';
 const router = express.Router();
 
 router.get('/listaralmacenes',almacen.listarAlmacenes);
+router.get('/listaralmacenesactivos',almacen.listarAlmacenesActivos);
+router.get('/listaralmacenesinactivos',almacen.listarAlmacenesInactivos);
+router.get('/agregaralmacenamien',almacen.agregarAlmacen);
+router.get('/editaralmacenamien',almacen.editarAlmacena);
+router.get('/eliminaralmacenamien',almacen.eliminarAlmacen);
+
+
+
 router.get('/listaralmacenamiento',almacen.listarAlmacenamiento);
+router.get('/agregaralmacenamiento',almacen.agregarAlmacenamiento);
+router.get('/editaralmacenamiento',almacen.editarAlmacenamiento);
+router.get('/eliminaralmacenamiento',almacen.eliminarAlmacenamiento);
+
 
 module.exports = router;
