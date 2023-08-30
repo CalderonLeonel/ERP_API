312d4232b984cc8d3aa299591fe8b22b40df55dc
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 11:07:35 -0400

    Agregando rutas para la gestion de proveedores

--- routes/proveedor.routes.js
+++ routes/proveedor.routes.js
@@ -4,5 +4,10 @@ import proveedor from '../controllers/prov';
 const router = express.Router();
 
 router.get('/listarproveedores',proveedor.listarproveedores);
+router.get('/listarproveedoresactivos',proveedor.listarProveedoresActivos);
+router.get('/listarproveedoresinactivos',proveedor.listarProveedoresInactivos);
+router.get('/insertar',proveedor.agregarProveedor);
+router.get('/editar',proveedor.actualizarProveedor);
+router.get('/anular',proveedor.eliminarProveedor);
 
 module.exports = router;
