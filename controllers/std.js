773d83f75c2bd87ba38291a94ea2b8cd2e5c8b92
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 11:00:06 -0400

    Agregando Listado de Activos/Inactivos

--- controllers/std.js
+++ controllers/std.js
@@ -20,6 +20,44 @@ stand.listarStands = async (req, res) => {
   }
 };
 
+stand.listarStandsActivos = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_stand_activo()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
+stand.listarStandsInactivos = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_stand_inactivo()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
 
 stand.agregarStand = async (req, res) => {
   const nombre = req.params.p1;
