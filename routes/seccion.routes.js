d85c0920b84ebba694ddac964269492270d3592f
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 11:07:43 -0400

    Agregando rutas para la gestion de secciones

--- routes/seccion.routes.js
+++ routes/seccion.routes.js
@@ -4,5 +4,9 @@ import seccion from '../controllers/sec';
 const router = express.Router();
 
 router.get('/listarsecciones',seccion.listarSecciones);
-
+router.get('/listarseccionesactivas',seccion.listarSeccionesActivas);
+router.get('/listarseccionesactivas',seccion.listarSeccionesInactivas);
+router.get('/agregarseccion',seccion.agregarSeccion);
+router.get('/actualizarseccion',seccion.actualizarSeccion);
+router.get('/eliminarseccion',seccion.eliminarSeccion);
 module.exports = router;
