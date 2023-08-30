c149face81cb97f95e906c1146de0ce02391fba2
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 11:07:49 -0400

    Agregando rutas para la gestion de stands

--- routes/stand.routes.js
+++ routes/stand.routes.js
@@ -4,5 +4,11 @@ import stand from '../controllers/std';
 const router = express.Router();
 
 router.get('/listarstands',stand.listarStands);
+router.get('/listarstandsactivos',stand.listarStandsActivos);
+router.get('/listarstandsinactivos',stand.listarStandsInactivos);
+router.get('/agregarstand',stand.agregarStand);
+router.get('/actualizarstand',stand.actualizarStand);
+router.get('/eliminarstand',stand.eliminarStand);
+
 
 module.exports = router;
