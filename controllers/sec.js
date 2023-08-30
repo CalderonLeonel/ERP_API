b29a4c975aa296e4dcbbd780421247512a929be0
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 11:00:02 -0400

    Agregando Listado de Activos/Inactivos

--- controllers/sec.js
+++ controllers/sec.js
@@ -20,6 +20,46 @@ seccion.listarSecciones = async (req, res) => {
   }
 };
 
+seccion.listarSeccionesActivas = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_seccion_activo()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
+
+seccion.listarSeccionesInactivas = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM erp_produccion.erp_listar_seccion_inactivo()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
+
 
 
 seccion.agregarSeccion = async(req,res) =>{
