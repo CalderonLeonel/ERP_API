003efd2fb28202d52ef83b13c259f41b2225866c
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 11:07:07 -0400

    Agregando rutas para la gestion de adquisiciones

--- routes/adquisicion.routes.js
+++ routes/adquisicion.routes.js
@@ -4,6 +4,21 @@ import adquisicion from '../controllers/adq';
 const router = express.Router();
 
 router.get('/listarcotizaciondeadquisicion',adquisicion.listarcotizacionadquisicion);
+router.get('/listarcotizaciondeadquisicionactiva',adquisicion.listarcotizacionadquisicionActiva);
+router.get('/listarcotizaciondeadquisicioninactiva',adquisicion.listarCotizacionAdquisicionAnulada);
+
+router.get('/agregarcotizacionadquisicion',adquisicion.agregarcotizacionadquisicion);
+router.get('/actualizarcotizacionadquisicion',adquisicion.actualizarcotizacionadquisicion);
+router.get('/eliminarcotizacionadquisicion',adquisicion.eliminarcotizacionadquisicion);
+
+
+
 router.get('/listarcotizacionitem',adquisicion.listarcotizacionitem);
+router.get('/listarcotizacionitemactiva',adquisicion.listarCotizacionItemActiva);
+router.get('/listarcotizacioniteminactiva',adquisicion.listarCotizacionItemAnulada);
+cotizacionitem
+router.get('/agregarcotizacionitem',adquisicion.agregarcotizacionitem);
+router.get('/actualizarcotizacionitem',adquisicion.actualizarcotizacionitem);
+router.get('/eliminarcotizacionitem',adquisicion.eliminarcotizacionitem);
 
 module.exports = router;
