f88bd067942c105236084ae44733ebc453864d57
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 23:10:40 -0400

    Correccion de bugs menores

--- routes/proveedor.routes.js
+++ routes/proveedor.routes.js
@@ -8,6 +8,6 @@ router.get('/listarproveedoresactivos',proveedor.listarProveedoresActivos);
 router.get('/listarproveedoresinactivos',proveedor.listarProveedoresInactivos);
 router.post('/insertar/:p1,:p2,:p3,:p4,:p5',proveedor.agregarProveedor);
 router.post('/editar/:p1,:p2,:p3,:p4,:p5,:p6',proveedor.actualizarProveedor);
-router.post('/anular/:p1,',proveedor.eliminarProveedor);
+router.post('/anular/:p1',proveedor.eliminarProveedor);
 
 module.exports = router;
