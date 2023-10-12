a7e83a31dfae07c80d9dbf0467cc41b546e49fd5
Brandon Montoya <MGJ2019761@est.univalle.edu>
2023-10-12 08:40:16 -0400

    Correccion de errores para la integracion

--- database/Keys.js
+++ database/Keys.js
@@ -1,7 +1,7 @@
 import {Pool} from 'pg';
 
 const pool = new Pool({
-    host : '34.31.174.19',
+    host : '10.102.48.3',
     port : '5432',
     user : 'postgres',
     password : "u-DM'%v-92TYq&m.",
