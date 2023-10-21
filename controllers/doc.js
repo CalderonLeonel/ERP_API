fab9a38e745a1cebefc3fe6a7b7048bc1d48308b
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-10-21 12:12:43 -0400

    Agregando metodo para la insercion de documentos

--- controllers/doc.js
+++ controllers/doc.js
@@ -20,6 +20,34 @@ documento.listardocumentos = async (req, res) => {
     }
   };
 
+  documento.agregarDocumento  = async(req,res) =>{
+    const nombreDocumento = req.params.p1;
+    const archivo = req.params.p2;
+    const tipoArchivo= req.params.p3;
+    const tamanioArchivo  = req.params.p4;
+    const descripcion = req.params.p5;
+    const codigo= req.params.p6;
+    const est  = req.params.p7;
+    
+    
+     try {
+           await pool.query("select proyectoerp.erp_insertar_cotizacion_adquisicion($1,$2,$3,$4,$5,$6,$7)",[nombreDocumento,archivo,tipoArchivo,tamanioArchivo,descripcion,codigo,est]);
+                             
+               res.status(200).json({
+                   message:'CAMPO GUARDADO CORRECTAMENTE :)'
+             
+               })
+           
+                      
+       } catch (error) {
+           res.status(500).json({
+               message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+               error
+           })
+       }
+   
+   };
+
 
   
 
