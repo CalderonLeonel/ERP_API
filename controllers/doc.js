689f2d4e0ea385526ca7ef3a01664db905ffa2c1
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-10-23 23:52:05 -0400

    Correccion de bugs

--- controllers/doc.js
+++ controllers/doc.js
@@ -1,6 +1,7 @@
 import pool from "../database/Keys";
 const documento = {};
 
+
 documento.listardocumentos = async (req, res) => {
     try {
       const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_documentos()")).rows;
@@ -24,14 +25,11 @@ documento.listardocumentos = async (req, res) => {
     const nombreDocumento = req.params.p1;
     const archivo = req.params.p2;
     const tipoArchivo= req.params.p3;
-    const tamanioArchivo  = req.params.p4;
-    const descripcion = req.params.p5;
     const codigo= req.params.p6;
     const est  = req.params.p7;
     
-    
      try {
-           await pool.query("select proyectoerp.erp_insertar_documento($1,$2,$3,$4,$5,$6,$7)",[nombreDocumento,archivo,tipoArchivo,tamanioArchivo,descripcion,codigo,est]);
+           await pool.query("select proyectoerp.erp_insertar_documento($1,$2,$3,$4,$5)",[nombreDocumento,archivo,tipoArchivo,codigo,est]);
                              
                res.status(200).json({
                    message:'CAMPO GUARDADO CORRECTAMENTE :)'
@@ -53,8 +51,6 @@ documento.listardocumentos = async (req, res) => {
     const id = req.params.p1;
     const nombreDocumento = req.params.p2;
     const archivo = req.params.p3;
-    const tipoArchivo= req.params.p4;
-    const tamanioArchivo  = req.params.p5;
     const descripcion = req.params.p6;
     const codigo= req.params.p7;
     const est  = req.params.p8;
@@ -62,7 +58,7 @@ documento.listardocumentos = async (req, res) => {
     
     
      try {
-           await pool.query("select proyectoerp.erp_actualizar_documento($1,$2,$3,$4,$5,$6,$7,$8)",[id,nombreDocumento,archivo,tipoArchivo,tamanioArchivo,descripcion,codigo,est]);
+           await pool.query("select proyectoerp.erp_actualizar_documento($1,$2,$3,$4,$5,$6",[id,nombreDocumento,archivo,descripcion,codigo,est]);
                              
                res.status(200).json({
                    message:'SE GUARDARON LOS CAMBIOS :)'
