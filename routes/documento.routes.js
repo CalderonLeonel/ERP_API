7696134bba2b761b2e5ce28397f0001646e36e54
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-10-25 21:07:25 -0400

    Actualizando las rutas y parametros

--- routes/documento.routes.js
+++ routes/documento.routes.js
@@ -4,8 +4,8 @@ import documento from '../controllers/doc';
 const router = express.Router();
 
 router.get('/listardocumentos',documento.listardocumentos);
-router.post('/insertar/:p1,:p2,:p3,:p4,:p5,:p6,:p7',documento.agregarDocumento);
-router.post('/editar/:p1,:p2,:p3,:p4,:p5,:p6,:p7,:p8',documento.actualizarDocumento);
+router.post('/insertar/:p1,:p2,:p3,:p4,:p5',documento.agregarDocumento);
+router.post('/editar/:p1,:p2,:p3,:p4,:p5,:p6',documento.actualizarDocumento);
 router.post('/anular/:p1',documento.eliminarDocumento);
 
 module.exports = router;
