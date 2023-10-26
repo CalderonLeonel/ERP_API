fdb5fc4ce324ca08986af2da136e7f75eb557e78
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-10-25 22:39:13 -0400

    Agregando metodo para descargar archivos

--- controllers/doc.js
+++ controllers/doc.js
@@ -98,6 +98,19 @@ documento.listardocumentos = async (req, res) => {
    
    };
 
+   documento.download = (req, res) => {
+    const fileName = req.params.name;
+    const directoryPath = __basedir + "/resources/static/assets/uploads/";
+  
+    res.download(directoryPath + fileName, fileName, (err) => {
+      if (err) {
+        res.status(500).send({
+          message: "Could not download the file. " + err,
+        });
+      }
+    });
+  };
+
 
   
 
