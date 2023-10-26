8a943330c7a1df2bd0335a1a59394791129a1fd4
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-10-25 23:32:13 -0400

    Agregando metodo para listar los archivos

--- controllers/doc.js
+++ controllers/doc.js
@@ -112,6 +112,31 @@ documento.listardocumentos = async (req, res) => {
   };
 
 
+  documento.getListFiles = (req, res) => {
+    const directoryPath = __basedir + "/archivos/documentos/";
+  
+    fs.readdir(directoryPath, function (err, files) {
+      if (err) {
+        res.status(500).send({
+          message: "Unable to scan files!",
+        });
+      }
+  
+      let fileInfos = [];
+  
+      files.forEach((file) => {
+        fileInfos.push({
+          name: file,
+          url: baseUrl + file,
+        });
+      });
+  
+      res.status(200).send(fileInfos);
+    });
+  };
+  
+
+
   
 
 module.exports = documento;
