55b09c3bfcb0dace0aa03c200edc2884554e54fb
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-10-25 22:39:47 -0400

    Agregando el metodo de descarga para el documento

--- routes/documento.routes.js
+++ routes/documento.routes.js
@@ -7,5 +7,6 @@ router.get('/listardocumentos',documento.listardocumentos);
 router.post('/insertar/:p1,:p2,:p3,:p4,:p5',documento.agregarDocumento);
 router.post('/editar/:p1,:p2,:p3,:p4,:p5,:p6',documento.actualizarDocumento);
 router.post('/anular/:p1',documento.eliminarDocumento);
+router.get('/descargar/:p1',documento.download);
 
 module.exports = router;
