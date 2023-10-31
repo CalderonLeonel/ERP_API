e97dae97848812d3ae7279296d2f5e050620ac55
Brandon Montoya <MGJ2019761@est.univalle.edu>
2023-10-31 08:43:24 -0400

    Modulo de Laboratorio terminado

--- controllers/prod.js
+++ controllers/prod.js
@@ -23,6 +23,50 @@ productos.listarproductos = async (req, res) => {
   }
 };
 
+productos.listarproductoslabo = async (req, res) => {
+  try {
+    const resultado = await (
+      await pool.query("select * from proyectoerp.erp_listarproductoslabo()")
+    ).rows;
+
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
+productos.listarobservaciones = async (req, res) => {
+  try {
+    const resultado = await (
+      await pool.query("select * from proyectoerp.erp_listarobservaciones()")
+    ).rows;
+
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
 productos.listarproductosinh = async (req, res) => {
   try {
     const resultado = await (
@@ -72,6 +116,57 @@ productos.addproducto = async (req, res) => {
   }
 };
 
+productos.addproductolabo = async (req, res) => {
+  const nomprod = req.params.p1;
+  const codprod = req.params.p2;
+  const idtipo = req.params.p3;
+  const idforma = req.params.p4;
+  const idfab = req.params.p5;
+  try {
+    await pool.query("select proyectoerp.erp_addproductolabo($1,$2,$3,$4,$5)", [
+      nomprod,
+      codprod,
+      idtipo,
+      idforma,
+      idfab,
+    ]);
+
+    res.status(200).json({
+      message: "SE GUARDARON LOS CAMBIOS!!!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
+productos.addobsproductolabo = async (req, res) => {
+  const obs = req.params.p1;
+  const idfab = req.params.p2;
+  const idusu = req.params.p3;
+  const idprod = req.params.p4;
+
+  try {
+    await pool.query("select proyectoerp.erp_addobsproductolabo($1,$2,$3,$4)", [
+      obs,
+      idfab,
+      idusu,
+      idprod,
+    ]);
+
+    res.status(200).json({
+      message: "SE GUARDARON LOS CAMBIOS!!!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
 productos.updproducto = async (req, res) => {
   const idprod = req.params.p1;
   const nomprod = req.params.p2;
@@ -80,7 +175,7 @@ productos.updproducto = async (req, res) => {
     await pool.query("select proyectoerp.erp_updproducto($1,$2,$3)", [
       idprod,
       nomprod,
-      codprod
+      codprod,
     ]);
 
     res.status(200).json({
