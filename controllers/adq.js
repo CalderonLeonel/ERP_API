20567bbb3a7749b96a169f5a53c77ec8fd6926e1
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-11-05 11:32:54 -0400

    Agregando metodo listarCotirzacionPendiente

--- controllers/adq.js
+++ controllers/adq.js
@@ -43,6 +43,25 @@ adquisicion.listarCotizacionAdquisicionActiva = async (req, res) => {
   }
 };
 
+adquisicion.listarCotizacionAdquisicionPendiente = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_cotizacion_adquisicion_pendiente()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
 adquisicion.listarCotizacionAdquisicionAnulada = async (req, res) => {
   try {
     const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_cotizacion_adquisicion_anulada()")).rows;
