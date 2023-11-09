eb9b79af6c48b77a2bdc5a361dc923b7bda42712
CalderonLeonel <crl1009575@est.univalle.edu>
2023-11-09 00:10:32 -0400

    CRUD Areas

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.use('/venta', require('./routes/venta.routes'));
 app.use('/cliente', require('./routes/cliente.routes'));
 app.use('/acceso', require('./routes/acceso.routes'));
 app.use('/turno', require('./routes/turno.routes'));
-
+app.use('/unidad', require('./routes/unidad.routes'));
+app.use('/area', require('./routes/area.routes'));
 
 app.use((req, res, next) => {
   console.log('Time:', Date.now())
