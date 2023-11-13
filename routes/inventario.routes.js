e0ea22780207fdad45a5f9b179807421d8bf0768
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-11-13 17:23:40 -0400

    Correccion de bugs

--- routes/inventario.routes.js
+++ routes/inventario.routes.js
@@ -32,6 +32,6 @@ router.post('/actualizartipodeitem/:p1,:p2,:p3',inventario.actualizarTipoItem);
 router.post('/eliminartipodeitem/:p1',inventario.eliminarTipoItem);
 
 
-router.get('/listarstanditem//:p1',inventario.listardetallestand);
+router.get('/listarstanditem/:p1',inventario.listardetallestand);
 
 module.exports = router;
