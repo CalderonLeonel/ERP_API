1b8eec5663a62bce15271ccd628e4157a453ba49
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-11-14 00:37:24 -0400

    Agregando metodo para obtener las existencias

--- controllers/inv.js
+++ controllers/inv.js
@@ -413,4 +413,25 @@ inventario.listarTipoItem = async (req, res) => {
 
 
 
+  inventario.listarExistencias = async (req, res) => {
+    try {
+      const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_obtener_existencias()")).rows;
+      if (resultado.length > 0) {
+        res.status(200).json({ resultado });
+      } else {
+        res.status(200).json({
+          message: "NO EXISTEN DATOS:(",
+          NotFount: true,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+        error,
+      });
+    }
+  };
+
+
+
 module.exports = inventario;
