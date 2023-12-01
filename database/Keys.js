30782764bee9b0c83103e418cfbc808222ceac65
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-12-01 19:14:10 -0400

    cambiando credenciales de la base de datos

--- database/Keys.js
+++ database/Keys.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     host : '34.42.159.150',
     port : '5432',
     user : 'postgres',
-    password : 'univalle',//"u-DM'%v-92TYq&m.",
+    password : "u-DM'%v-92TYq&m.",
     database :'erp_produccion'
 });
 
