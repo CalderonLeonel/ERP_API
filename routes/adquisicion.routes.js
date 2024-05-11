8c7eca64af85331820754c7d9d2b64f9afebe286
SebastianClavijoA <cag5000404@est.univalle.edu>
2024-05-11 03:53:50 -0400

    Actualizando las rutas para actualizar los archivos de adquisiciones y proveedor

--- routes/adquisicion.routes.js
+++ routes/adquisicion.routes.js
@@ -8,8 +8,9 @@ router.get('/listarcotizaciondeadquisicionactiva',adquisicion.listarCotizacionAd
 router.get('/listarcotizaciondeadquisicionpendiente',adquisicion.listarCotizacionAdquisicionPendiente);
 router.get('/listarcotizaciondeadquisicioninactiva',adquisicion.listarCotizacionAdquisicionAnulada);
 
-router.post('/agregarcotizacionadquisicion/:p1,:p2,:p3,:p4,:p5',adquisicion.agregarcotizacionadquisicion);
+router.post('/agregarcotizacionadquisicion/:p1,:p2,:p3,:p4,:p5,:p6',adquisicion.agregarcotizacionadquisicion);
 router.post('/actualizarcotizacionadquisicion/:p1,:p2,:p3,:p4,:p5,:p6',adquisicion.actualizarcotizacionadquisicion);
+router.post('/actualizarcotizacionadquisicionarchivo/:p1,:p2,:p3,:p4,:p5,:p6,:p7',adquisicion.actualizarcotizacionadquisicionarchivo);
 router.post('/eliminarcotizacionadquisicion/:p1',adquisicion.eliminarcotizacionadquisicion);
 
 
