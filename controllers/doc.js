e1d1993eec27fb05ab80b4cded5385989da40925
SebClav <sebasclavijoalessandri@gmail.com>
2024-05-20 05:27:32 -0400

    Agregando metodo para listar los documentos de las cotizaciones

--- controllers/doc.js
+++ controllers/doc.js
@@ -251,6 +251,25 @@ documento.getFiredEmployees = async (req, res) => {
     });
   }
 };
+
+documento.listardocumentosadquisicion = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_adquisicion_archivo()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
   
 
 module.exports = documento;
