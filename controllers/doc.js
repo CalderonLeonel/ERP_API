c69055b88f40d608b763e33b283eff61ea8bbb91
SebClav <sebasclavijoalessandri@gmail.com>
2024-05-20 05:28:54 -0400

    Agregando metodo para listar los documentos de los contratos

--- controllers/doc.js
+++ controllers/doc.js
@@ -289,6 +289,25 @@ documento.listardocumentosproveedor = async (req, res) => {
     });
   }
 };
+
+documento.listardocumentosalerta = async (req, res) => {
+  try {
+    const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_contrato_archivo()")).rows;
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS:(",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+      error,
+    });
+  }
+};
   
 
 module.exports = documento;
