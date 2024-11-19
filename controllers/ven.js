e09a775e0e12613c1c0b660f782bfeef8fd1ac27
SebastianClavijoA <cag5000404@est.univalle.edu>
2024-11-19 19:02:10 -0400

    Add GET method to retrieve venta details

--- controllers/ven.js
+++ controllers/ven.js
@@ -24,6 +24,29 @@ ventas.listarventas = async (req, res) => {
   }
 };
 
+ventas.listardetalleventas = async (req, res) => {
+  try {
+    const id_venta = req.params.p1;
+    const resultado = await (
+      await pool.query("select * from proyectoerp.erp_listardetalleventa($1)",[id_venta]).rows);
+
+    if (resultado.length > 0) {
+      res.status(200).json({ resultado });
+    } else {
+      res.status(200).json({
+        message: "NO EXISTEN DATOS",
+        NotFount: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message:
+        "ERROR INESPERADO REPORTELO AL DEPARTAMENTO DE SISTEMAS, GRACIAS !!!",
+      error,
+    });
+  }
+};
+
 ventas.listarventasinh = async (req, res) => {
   try {
     const resultado = await (
