b98c9ea8e7ebec574fa6d49eb71e08272e2a203f
Brandon Montoya <MGJ2019761@est.univalle.edu>
2025-08-06 20:32:43 -0400

    Correcion de Bugs de Snackbars

--- database/Keys.js
+++ database/Keys.js
@@ -13,8 +13,8 @@ const pool = new Pool({
     host : '127.0.0.1',
     port : '5432',
     user : 'postgres',
-    password : "univalle",
-    database :'proyectoerp'
+    password : "developerlover69",
+    database :'erp_produccion'
 });
 
 
