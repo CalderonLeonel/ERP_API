99a11d87cd96ff2bd6ea80887afccf2365251cf8
SebastianClavijoA <cag5000404@est.univalle.edu>
2025-08-20 15:24:38 -0400

    Add subcategorY CRUD methods

--- controllers/inv.js
+++ controllers/inv.js
@@ -572,6 +572,67 @@ inventario.listarCategoria = async (req, res) => {
    };
 
 
+   //Subcategoria
+
+
+   inventario.listarSubcategoria = async (req, res) => {
+    try {
+      const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_subcategoria()")).rows;
+      if (resultado.length > 0) {
+        res.status(200).json({ resultado });
+      } else {
+        res.status(200).json({
+          message: "NO EXISTEN DATOS:(",
+          NotFount: true,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+        error,
+      });
+    }
+  };
+
+  inventario.listarSubcategoriaActivo = async (req, res) => {
+    try {
+      const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_subcategoria_activo()")).rows;
+      if (resultado.length > 0) {
+        res.status(200).json({ resultado });
+      } else {
+        res.status(200).json({
+          message: "NO EXISTEN DATOS:(",
+          NotFount: true,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+        error,
+      });
+    }
+  };
+
+  inventario.listarSubcategoriaInactivo = async (req, res) => {
+    try {
+      const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_subcategoria_inactivo()")).rows;
+      if (resultado.length > 0) {
+        res.status(200).json({ resultado });
+      } else {
+        res.status(200).json({
+          message: "NO EXISTEN DATOS:(",
+          NotFount: true,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+        error,
+      });
+    }
+  };
+
+
    
    inventario.agregarSubcategoria = async (req, res) => {
 
