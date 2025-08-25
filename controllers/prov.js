727b011a5798a2317533fd12ffe23ffe5d1a1c28
SebastianClavijoA <cag5000404@est.univalle.edu>
2025-08-24 21:00:10 -0400

    Add provider category CRUD methods

--- controllers/prov.js
+++ controllers/prov.js
@@ -178,4 +178,156 @@ proveedor.agregarProveedorArchivo = async(req,res) =>{
  };
 
 
+ //Categorias Proveedor
+
+ 
+ proveedor.listarCategoria = async (req, res) => {
+     try {
+       const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_categoria_proveedor()")).rows;
+       if (resultado.length > 0) {
+         res.status(200).json({ resultado });
+       } else {
+         res.status(200).json({
+           message: "NO EXISTEN DATOS:(",
+           NotFount: true,
+         });
+       }
+     } catch (error) {
+       res.status(500).json({
+         message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+         error,
+       });
+     }
+   };
+ 
+   proveedor.listarCategoriaActivo = async (req, res) => {
+     try {
+       const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_categoria_proveedor_activo()")).rows;
+       if (resultado.length > 0) {
+         res.status(200).json({ resultado });
+       } else {
+         res.status(200).json({
+           message: "NO EXISTEN DATOS:(",
+           NotFount: true,
+         });
+       }
+     } catch (error) {
+       res.status(500).json({
+         message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+         error,
+       });
+     }
+   };
+ 
+   proveedor.listarCategoriaInactivo = async (req, res) => {
+     try {
+       const resultado = await(await pool.query("SELECT * FROM proyectoerp.erp_listar_categoria_proveedor_inactivo()")).rows;
+       if (resultado.length > 0) {
+         res.status(200).json({ resultado });
+       } else {
+         res.status(200).json({
+           message: "NO EXISTEN DATOS:(",
+           NotFount: true,
+         });
+       }
+     } catch (error) {
+       res.status(500).json({
+         message: "INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!",
+         error,
+       });
+     }
+   };
+ 
+   
+ 
+   proveedor.agregarCategoria = async (req, res) => {
+ 
+     const nombre = req.params.p1;
+     const est  = req.params.p2;
+     try {
+       await pool.query("select proyectoerp.erp_insertar_categoria_proveedor($1,$2)",[nombre,est]);
+                         
+           res.status(200).json({
+               message:'CAMPO GUARDADO CORRECTAMENTE :)'
+         
+           })           
+     } catch (error) {
+         res.status(500).json({
+             message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+             error
+         })
+     }
+   
+   
+   };
+ 
+ 
+   
+   proveedor.actualizarCategoria = async (req, res) => {
+     const id = req.params.p1;
+     const nombre = req.params.p2;
+     const est  = req.params.p3;
+     try {
+       await pool.query("select proyectoerp.erp_actualizar_categoria_proveedor($1,$2,$3)",[id,nombre,est]);
+                         
+           res.status(200).json({
+               message:'CAMPO GUARDADO CORRECTAMENTE :)'
+         
+           })           
+     } catch (error) {
+         res.status(500).json({
+             message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+             error
+         })
+     }
+   
+   
+   };
+ 
+   proveedor.eliminarCategoria = async(req,res) =>{
+     const id = req.params.p1;
+   
+   
+      try {
+            await pool.query("select proyectoerp.erp_eliminar_categoria_proveedor($1)",[id]);
+                              
+                res.status(200).json({
+                    message:'ESTADO CAMBIADO CORRECTAMENTE'
+              
+                })
+            
+                       
+        } catch (error) {
+            res.status(500).json({
+                message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+                error
+            })
+        }
+    
+    };
+ 
+    
+   proveedor.activarCategoria = async(req,res) =>{
+     const id = req.params.p1;
+   
+   
+      try {
+            await pool.query("select proyectoerp.erp_activar_categoria_proveedor($1)",[id]);
+                              
+                res.status(200).json({
+                    message:'ESTADO CAMBIADO CORRECTAMENTE'
+              
+                })
+            
+                       
+        } catch (error) {
+            res.status(500).json({
+                message:'INESPERADO ERROR REPORTELO A ASI INMEDIATAMENTE, GRACIAS !!!',
+                error
+            })
+        }
+    
+    };
+
+
 module.exports = proveedor;
