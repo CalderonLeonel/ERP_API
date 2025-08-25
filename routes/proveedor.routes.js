3f31db1dbbfc38471a3cc38daaf16a6f3eb92899
SebastianClavijoA <cag5000404@est.univalle.edu>
2025-08-24 21:00:25 -0400

    Add provider category CRUD http routes

--- routes/proveedor.routes.js
+++ routes/proveedor.routes.js
@@ -12,4 +12,14 @@ router.post('/editar/:p1,:p2,:p3,:p4,:p5,:p6',proveedor.actualizarProveedor);
 router.post('/editararchivo/:p1,:p2,:p3,:p4,:p5,:p6,:p7',proveedor.actualizarProveedorarchivo);
 router.post('/anular/:p1',proveedor.eliminarProveedor);
 
+
+router.get('/listarcategoria', proveedor.listarCategoria);
+router.get('/listarcategoriaactivo', proveedor.listarCategoriaActivo);
+router.get('/listarcategoriainactivo', proveedor.listarCategoriaInactivo);
+
+router.post('/agregarcategoria/:p1,:p2', proveedor.agregarCategoria);
+router.post('/actualizarcategoria/:p1,:p2,:p3', proveedor.actualizarCategoria);
+router.post('/eliminarcategoria/:p1', proveedor.eliminarCategoria);
+router.post('/activarcategoria/:p1', proveedor.activarCategoria);
+
 module.exports = router;
